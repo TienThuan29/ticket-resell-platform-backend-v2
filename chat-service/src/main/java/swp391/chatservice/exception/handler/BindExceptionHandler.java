48e15bca7a51ca2e1650f755b4afe8717f7d0633
2109TruongNguyen <nntnnt2000@gmail.com>
2024-10-21 19:31:20 +0700

    no comment

--- chat-service/src/main/java/swp391/chatservice/exception/handler/BindExceptionHandler.java
+++ chat-service/src/main/java/swp391/chatservice/exception/handler/BindExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import swp391.ticketservice.exception.def.ErrorResponse;
+import swp391.chatservice.exception.def.ErrorResponse;
 
 @RestControllerAdvice(name = "ticketServiceBindExceptionHandler")
 public class BindExceptionHandler {
