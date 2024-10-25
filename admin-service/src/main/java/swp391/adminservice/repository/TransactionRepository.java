a17ddc4018fb9de87d82d7acd4610931ec211804
conghuy113 <conghuy.fptu@gmail.com>
2024-10-25 21:29:35 +0700

    code Admin to get sum of Transaction and count tickets

--- admin-service/src/main/java/swp391/adminservice/repository/TransactionRepository.java
+++ admin-service/src/main/java/swp391/adminservice/repository/TransactionRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import swp391.entity.Transaction;
 import swp391.entity.fixed.TransactionType;
 
-import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     @Query("SELECT sum(t.amount) from Transaction t where t.type =:type and t.isDone = true")
